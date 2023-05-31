93c9a500ca62d7ba654d821915c76770c473c4d5
laurasofia4908 <135129067+laurasofia4908@users.noreply.github.com>
2023-05-31 08:43:33 -0500

    Update Sum_Python.py

--- Sum_Python.py
+++ Sum_Python.py
@@ -1,22 +1,6 @@
-import requests
 
-def obtener_capital(pais):
-    url = f"https://restcountries.com/v2/name/{pais}?fullText=true"
+animales = ["canguro", "cocodrilo", "camello", "cerdo", "cabra"]
 
-    try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            datos = response.json()
-            capital = datos[0]['capital']
-            return capital
-        else:
-            print("Error en la solicitud. Código de estado:", response.status_code)
-    except requests.exceptions.RequestException as e:
-        print("Error en la solicitud:", e)
-
-# Ejemplo de uso
-pais = input("Ingresa el nombre del país: ")
-capital = obtener_capital(pais)
-
-if capital is not None:
-    print(f"La capital de {pais} es {capital}.")
+for animal in animales:
+    if animal.startswith("c"):
+        print(animal)
